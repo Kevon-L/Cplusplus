cd6008fea621112b99da6b093d65fe5e81afd356
KeVon <kevonjl@email.sc.edu>
2026-06-15 16:32:24 -0400

    Created the map

--- Snake/main.cc
+++ Snake/main.cc
@@ -7,12 +7,44 @@ using std::endl;
 
 bool gameOver;
 
+const int width = 20;
+const int height = 20;
+
+int x, y, fruitX, fruitY, score;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
 void Setup() {
     gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 }
 
 void Draw() {
-    
+    system("clear");
+
+    for(int i = 0; i < width + 2; i++) 
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width + 2; j++) {
+            if (j == 0 || j == width + 1) //if it's a part of the left or right border
+                cout << "#";
+            else
+                cout << " ";
+        }
+        
+        cout << endl;
+    }
+
+    for(int i = 0; i < width + 2; i++) 
+        cout << "#";
+    cout << endl;
 }
 
 void Input() {
