12f1495b351e98276e63dad193e0a9bcdd96ee5e
KeVon <kevonjl@email.sc.edu>
2026-06-15 15:53:17 -0400

    basic setup for games

---
+++ Snake/main.cc
@@ -0,0 +1,36 @@
+// Copyright 2026 KeVon Lindsey
+
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+bool gameOver;
+
+void Setup() {
+    gameOver = false;
+}
+
+void Draw() {
+    
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
+int main() {
+    Setup();
+
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+    }
+
+    return 0;
+}
