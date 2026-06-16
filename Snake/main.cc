b83fb6a46167363a2d0b9f18441c75f6b82c6f79
KeVon <kevonjl@email.sc.edu>
2026-06-15 22:46:01 -0400

    snake movement completed

--- Snake/main.cc
+++ Snake/main.cc
@@ -1,6 +1,9 @@
 // Copyright 2026 KeVon Lindsey
 
 #include<iostream>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
 using std::cout;
 using std::cin;
 using std::endl;
@@ -13,6 +16,50 @@ const int height = 20;
 int x, y, fruitX, fruitY, score;
 enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
+
+int kbhit()
+{
+    termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
+
+char getch()
+{
+    termios oldt, newt;
+    char ch;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    read(STDIN_FILENO, &ch, 1);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+    return ch;
+}
 
 void Setup() {
     gameOver = false;
@@ -27,32 +74,80 @@ void Setup() {
 void Draw() {
     system("clear");
 
-    for(int i = 0; i < width + 2; i++) 
+    for (int i = 0; i < width + 2; i++)
         cout << "#";
     cout << endl;
 
     for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width + 2; j++) {
-            if (j == 0 || j == width + 1) //if it's a part of the left or right border
+        for (int j = 0; j < width; j++) {
+            if (j == 0 ) // left border
                 cout << "#";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
             else
                 cout << " ";
+
+            if (j == width - 1)
+                cout << "#";
         }
-        
+
         cout << endl;
     }
 
-    for(int i = 0; i < width + 2; i++) 
+    for(int i = 0; i < width + 2; i++)
         cout << "#";
     cout << endl;
 }
 
-void Input() {
+void Input()
+{
+    if (kbhit())
+    {
+        switch (getch())
+        {
+            case 'a':
+                dir = LEFT;
+                break;
 
+            case 'd':
+                dir = RIGHT;
+                break;
+
+            case 'w':
+                dir = UP;
+                break;
+
+            case 's':
+                dir = DOWN;
+                break;
+
+            case 'x':
+                gameOver = true;
+                break;
+        }
+    }
 }
 
 void Logic() {
-
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
 }
 
 int main() {
