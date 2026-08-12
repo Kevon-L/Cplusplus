5a648dd3267ea895414c5df1893bfa4c48cdcbeb
KeVon <kevonjl@email.sc.edu>
2026-08-12 12:14:53 -0400

    Added neccesarry header files to main

--- Bicycle/main.cc
+++ Bicycle/main.cc
@@ -2,7 +2,9 @@
 
 #include <iostream>
 #include <string>
-#include "Zodiacfunctions.h"
+#include"Bicycle.h"
+#include"Frame.h"
+#include"Wheel.h"
 
 using std::cin;
 using std::cout;
@@ -15,7 +17,7 @@ int main() {
     // Default Bike
     cout << "First we will create a \"Default\" Bicycle" << endl;
 	
-    Bicycle b01 = new Bicycle();
+    Bicycle b01;
 	
     cout << "Printing the Bicycle's data" << endl;
 	cout << "Bicycle's make: " << b01.getMake() << endl;
@@ -23,29 +25,7 @@ int main() {
 	cout << "Bicycle's back wheel: Diameter "+b01.getBackWheel().getDiameter()+" Width: "+b01.getBackWheel().getWidth() << endl;
 	cout << "Bicycle's frame: Size: "+b01.getFrame().getSize()+" Type: "+b01.getFrame().getType() << endl;
 	cout << "\nTesting the toString method" << endl;
-	cout << b01 << endl;
-		
-	//Setting wrong properties.
-	cout << "\nSetting invalid values for the default bike's wheels and frame" << endl;
-	b01.setBackWheel(new Wheel(0,0));
-	b01.setFrontWheel(new Wheel(10000,10000));
-	b01.setFrame(new Frame(0.0, "hexagon"));
-	cout << b01 << endl;
-
-	//Param Bike
-	cout << "\nCreating another bike using the parameterized constructor" << endl;
-	Bicycle b02 = new Bicycle("Big Wheel", new Wheel(55.0,2.5), new Wheel(18.0,2.0),new Frame(60.0, "Penny-Farthing"));
-	cout << b02 << endl;
-	
-	cout << "\nCreating another bike using the parameterized constructor with invalid values" << endl;
-	Bicycle b03 = new Bicycle(null, new Wheel(555.0,-2.5), new Wheel(-18.0,22.0),new Frame(-60.0, "dirt"));
-	cout << b03 << endl;
-	
-	//Equals
-	cout << "\nChecking the \"equals\" method" << endl;
-	cout << "Does the first and third bicycles have different memory addresses? " << (b01!=b03) << endl;
-	cout << "Does the first and third bicycles have the same properties? " << b01.equals(b03) << endl;
-	cout << "\n---------------------------------\nTests Complete! Goodbye!\n---------------------------------" << endl;
+	cout << b01.toString() << endl;
 
     return 0;
 }
